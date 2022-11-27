7045171e58a51b6abccc032acdd27ff8e51a8b28
guiriguy <connorpin619@gmail.com>
2022-11-27 13:10:39 +0100

    mod.info

--- Contents/mods/MoreSkills - Dexterity/media/lua/client/MSDexCode.lua
+++ Contents/mods/MoreSkills - Dexterity/media/lua/client/MSDexCode.lua
@@ -5,6 +5,8 @@ local MSDexterity = {}
 MSDexterity.modData = nil
 MSDexterity.OGCode_ISITAStart =  ISInventoryTransferAction.start
 MSDexterity.OGCode_ISITAPerform = ISInventoryTransferAction.perform
+local Timer = 0
+local Counting = false
 
 --╭────────────────────────╮
 --|       Functions        |
@@ -12,6 +14,9 @@ MSDexterity.OGCode_ISITAPerform = ISInventoryTransferAction.perform
 MSDexterity.CheckPerkLvl = function(player)
     if player:getXp():getXP(Perks.Dexterity) < 10500 and not player:HasTrait("AllThumbs") then
         player:getTraits():add("AllThumbs")
+        if player:HasTrait("Dextrous") then
+            player:getTraits():remove("Dextrous")
+        end
     elseif player:getXp():getXP(Perks.Dexterity) >= 10500
             and player:getXp():getXP(Perks.Dexterity) <= 127500
             and (player:HasTrait("AllThumbs")
@@ -21,6 +26,9 @@ MSDexterity.CheckPerkLvl = function(player)
         player:getTraits():remove("Dextrous")
     elseif player:getXp():getXP(Perks.Dexterity) >= 127500 and not player:HasTrait("Dextrous") then
         player:getTraits():add("Dextrous")
+        if player:HasTrait("AllThumbs") then
+            player:getTraits():remove("AllThumbs")
+        end
     end
     --[[if _player:getXp():getXP(Perks.Dexterity) > 10500 then
         _player:getTraits():add("AllThumbs")
@@ -47,10 +55,40 @@ MSDexterity.GetTime = function(player,time)
     --print("Salen: "..time)
     return time
 end
+MSDexterity.giveXP = function(player, weight, container)
+    local tWeight = player:getModData().MSDexWeightToday
+    if not tWeight then
+        player:getModData().MSDexWeightToday = 0
+    end
+    if container == "floor" then
+        player:getXp():AddXP(Perks.Dexterity, (weight))
+    else
+        player:getXp():AddXP(Perks.Dexterity, (weight*4))
+    end
+    print(player:getModData().MSDexWeightToday)
+    player:getModData().MSDexWeightToday = player:getModData().MSDexWeightToday + weight
+    print(player:getModData().MSDexWeightToday)
+    if player:getModData().MSDexWeightToday > 0 and player:getModData().MSDexWeightToday <= 1000 then
+        player:getXp():addXpMultiplier(Perks.Dexterity,((14*((player:getPerkLevel(Perks.Dexterity)+1)/10))*(player:getModData().MSDexWeightToday/1000))+1,player:getPerkLevel(Perks.Dexterity),10)
+        print(((14*((player:getPerkLevel(Perks.Dexterity)+1)/10))*(player:getModData().MSDexWeightToday/1000))+1)
+    elseif player:getModData().MSDexWeightToday > 1000 and player:getModData().MSDexWeightToday < 2000 then
+        player:getXp():addXpMultiplier(Perks.Dexterity,(((14*((player:getPerkLevel(Perks.Dexterity)+1)/10))+1)-(((14*((player:getPerkLevel(Perks.Dexterity)+1)/10))*((player:getModData().MSDexWeightToday-1000)/1000))+1)),player:getPerkLevel(Perks.Dexterity),10)
+        print(((14*((player:getPerkLevel(Perks.Dexterity)+1)/10))+1)-(((14*((player:getPerkLevel(Perks.Dexterity)+1)/10))*((player:getModData().MSDexWeightToday-1000)/1000))+1))
+    elseif player:getModData().MSDexWeightToday > 2000 then
+        player:getXp():addXpMultiplier(Perks.Dexterity,0,player:getPerkLevel(Perks.Dexterity),10)
+    end
+end
+MSDexterity.ResettWeight = function()
+    for i = 0,getNumActivePlayers()-1 do
+        local _player = getSpecificPlayer(i)
+        if _player and not _player:isDead() then
+            _player:getModData().MSDexWeightToday = 0
+        end
+    end
+end
 --╭────────────────────────╮
 --|          Code          |
 --╰────────────────────────╯
-
 function ISInventoryTransferAction:start()
     local _player = self.character
     MSDexterity.OGCode_ISITAStart(self)
@@ -64,11 +102,7 @@ end
 
 function ISInventoryTransferAction:perform()
     local _player = self.character
-    if self.destContainer:getType() == "floor" then
-        _player:getXp():AddXP(Perks.Dexterity, (self.item:getActualWeight()*4))
-    else
-        _player:getXp():AddXP(Perks.Dexterity, (self.item:getActualWeight()*16))
-    end
+    MSDexterity.giveXP(_player,self.item:getActualWeight(),self.destContainer:getType())
     MSDexterity.CheckPerkLvl(_player)
     MSDexterity.OGCode_ISITAPerform(self)
     if #self.queueList > 0 then
@@ -81,3 +115,20 @@ end
 --╭────────────────────────╮
 --|         Events         |
 --╰────────────────────────╯
+Events.EveryDays.Add(MSDexterity.ResettWeight)
+local Yes = true
+local function test()
+    local Calendar = Calendar.getInstance():getTime()
+    local Time = Calendar:getTime()
+    local Timer = Calendar:getTimeInMillis()/1000
+    if Seconds == Timer and Givethen
+        Timer = Seconds + 3
+        Counting = true
+    elseif Counting then
+        Timer = Seconds
+    end
+    print("Times is: "..tostring(Time))
+end
+if Yes then
+    Events.OnPlayerUpdate.Add(test)
+end
